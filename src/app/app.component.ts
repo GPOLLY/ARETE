0b0160b898b1550187dd5c00456a897e58ee2ad0
Gpolly <gngugi17@gmail.com>
2020-04-03 22:42:03 +0300

    this is a push of a contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Arete';
+
+
+form = new FormGroup({
+  name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  email: new FormControl('', [Validators.required, Validators.email]),
+  body: new FormControl('', Validators.required)
+});
+ 
+get f(){
+    return this.form.controls;
+}
+ 
+submit(){
+    if(this.form.status === 'VALID'){
+      console.log(this.form.value);
+    }
+}
+
+setValue(){
+    this.form.setValue({name: 'Hardik Savani', email: 'itsolutionstuff@gmail.com', body: 'This is testing from itsolutionstuff.com'});
+}
+
+resetValue(){
+    this.form.reset({name: '', email: '', body: ''});
+}
+  
 }
