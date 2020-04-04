c8592fa6a4c71c965e2736258a10e568bf838571
Gpolly <gngugi17@gmail.com>
2020-04-04 10:57:40 +0300

    Update the contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 
@@ -9,12 +10,14 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 export class AppComponent {
   title = 'Arete';
 
+ 
 
 form = new FormGroup({
   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
   email: new FormControl('', [Validators.required, Validators.email]),
   body: new FormControl('', Validators.required)
 });
+// end of the google map
  
 get f(){
     return this.form.controls;
